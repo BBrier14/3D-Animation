c8287a06b7be989fa7561544b3f65b0a7fdba963
Benjamin Brier <ben.brier14@gmail.com>
2020-04-21 13:57:55 -0400

    added lighting, background, fixed broken docs, etc

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ let house;
 function init() {
 	container = document.querySelector('.scene');
 
-	//Create Scene
+	//Create scene
 	scene = new THREE.Scene();
 
 	const fov = 35;
 	const aspect = container.clientWidth / container.clientHeight;
 	const near = 0.1;
-	const far = 500;
+	const far = 1000;
 
-	//Camera Setup
+	//Camera setup
 	camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-	camera.position.set(0, 0, 50);
+	camera.position.set(0, 5, 30);
 
+	const ambient = new THREE.AmbientLight(0x404040, 2);
+	scene.add(ambient);
+
+	const light = new THREE.DirectionalLight(0xffffff, 2);
+	light.position.set(50, 50, 100);
+	scene.add(light);
 	//Renderer
 	renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 	renderer.setSize(container.clientWidth, container.clientHeight);
@@ -32,8 +38,24 @@ function init() {
 	let loader = new THREE.GLTFLoader();
 	loader.load('./house/scene.gltf', function(gltf) {
 		scene.add(gltf.scene);
-		renderer.render(scene, camera);
+		house = gltf.scene.children[0];
+		animate();
 	});
 }
 
+function animate() {
+	requestAnimationFrame(animate);
+	house.rotation.z += 0.005;
+	renderer.render(scene, camera);
+}
+
 init();
+
+function onWindowResize() {
+	camera.aspect = container.clientWidth / container.clientHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener('resize', onWindowResize);
